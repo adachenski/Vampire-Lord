3f9414e1cf562daf70880a1db09c1ab54ae6c74a
missytoy <missytoy@gmail.com>
2015-02-26 18:35:45 +0200

    add

---
+++ SnakeCSharp/SnakeCSharp/SnakeCSharp/GameObject.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeCSharp
+{
+    public class GameObject : IEquatable<GameObject>
+    {
+        public GameObject(int horizontal, int vertical)
+        {
+            this.Horizontal = horizontal;
+            this.Vertical = vertical;
+        }
+
+        public int Horizontal { get; set; }
+
+        public int Vertical { get; set; }
+
+        public ConsoleColor Color { get; set; }
+
+        public bool Equals(GameObject other)
+        {
+            return this.Horizontal.Equals(other.Horizontal) &&
+                this.Vertical.Equals(other.Vertical);
+        }
+
+        public void Print(char symbol)
+        {
+            Console.SetCursorPosition(this.Horizontal, this.Vertical);
+            Console.Write(symbol);
+        }
+
+        public void Print(string str)
+        {
+            Console.SetCursorPosition(this.Horizontal, this.Vertical);
+            Console.Write(str);
+        }
+    }
+}
